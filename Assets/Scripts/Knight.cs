7e27399d7e8a906518f10e82eb7cf74ae5a201f8
warnockr <warnockr@sheridancollege.ca>
2025-03-27 14:53:41 -0400

    Gym11

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Knight : MonoBehaviour
 {
+    AudioSource audio;
 
     SpriteRenderer sr;
     Animator animator;
     public float speed = 2;
+    public List<AudioClip> steps;
     public bool canRun = true;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -47,5 +50,17 @@ public class Knight : MonoBehaviour
         Debug.Log("The attack has finished");
         canRun = true;
     }
+
+
+
+    public void footLanded()
+    {
+        audio.clip = steps[(int)Random.Range(0,4)];
+        audio.Play();
+
+
+    }
+
+
 
 }
