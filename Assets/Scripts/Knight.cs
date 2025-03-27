baddf41dcd42a78c206a38f8e2a8f64d1122fe35
warnockr <warnockr@sheridancollege.ca>
2025-03-27 14:02:56 -0400

    Animations Assets and Playing with Animations

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    Animator animator;
+    public float speed = 2;
+    public bool canRun = true;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+
+
+        sr.flipX = (direction < 0);
+        animator.SetFloat("Movement",Mathf.Abs(direction));
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+            canRun = false;
+        }
+
+
+
+       if (canRun == true) { 
+        transform.position += transform.right * direction * speed * Time.deltaTime;
+        }
+
+
+    }
+
+
+    public void AttackHasFinished()
+    {
+        Debug.Log("The attack has finished");
+        canRun = true;
+    }
+
+}
